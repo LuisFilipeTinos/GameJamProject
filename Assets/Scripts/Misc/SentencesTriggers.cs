6b6f7382af8e6a8782e814919ff2f6a30b281f0b
LuisFilipeTinos <lftinos@gmail.com>
2023-02-26 23:25:12 -0300

    Chão fake, ajustes de cenário pra poder ir e voltar, ajuste de altura de câmera e mais trigger antes de abrir o segundo portão

--- Assets/Scripts/Misc/SentencesTriggers.cs
+++ Assets/Scripts/Misc/SentencesTriggers.cs
@@ -10,6 +10,8 @@ public class SentencesTriggers : MonoBehaviour
     [SerializeField] TextMeshProUGUI sentenceText;
     [SerializeField] GameObject sentenceContainer;
     bool isVisible;
+
+    [SerializeField] PlayerController playerController; 
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,11 @@ public class SentencesTriggers : MonoBehaviour
     private void Update()
     {
         if (isVisible && Input.GetKeyDown(KeyCode.Z))
+        {
             sentenceContainer.SetActive(false);
+            playerController.canMove = true;
+            playerController.rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -30,11 +36,18 @@ public class SentencesTriggers : MonoBehaviour
         {
             sentenceContainer.SetActive(true);
             isVisible = true;
+            playerController.canMove = false;
+            playerController.rb2d.velocity = Vector2.zero;
+            playerController.rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
 
             if (this.gameObject.name == "FirstSentenceTrigger")
             {
                 sentenceText.text = "- Você vai ficar bem mamãe?" + Environment.NewLine +
                                     "- Vou sim meu amor, o médico disse que eu preciso fazer o tratamento e descansar.";
+            }
+            else if (this.gameObject.name == "SecondSentenceTrigger")
+            {
+                sentenceText.text = "- Não estou com um bom pressentimento. Acho melhor achar alguma coisa pra me defender.";
             }
         }
     }
